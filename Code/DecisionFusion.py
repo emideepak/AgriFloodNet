a7aa97a4422652f3b27231df71ba0d27c2ae7e49
emideepak <79080929+emideepak@users.noreply.github.com>
2021-06-01 11:47:19 +0530

    Update DecisionFusion.py

--- Code/DecisionFusion.py
+++ Code/DecisionFusion.py
@@ -1,7 +1,7 @@
 ##################################################################################
 # Program: Generating overlaid images for qualitative analysis of patches 
 # Inputs:  S1 ans S2 Images generated from AgrifloodNet 
-# Output:  Combined before and after map of S1 and S2 images (Combined_Before_map.png)
+# Output:  Combined before and after map of S1 and S2 images (Combined_Before_Flood_map.png)
 ###################################################################################
 try:
     from PIL import Image
@@ -15,4 +15,4 @@ background = background.convert("RGBA")
 overlay = overlay.convert("RGBA")
 
 new_img = Image.blend(background, overlay, 0.5)
-new_img.save("Combined_Before_map.png","PNG")
+new_img.save("Combined_Before_Flood_map.png","PNG")
