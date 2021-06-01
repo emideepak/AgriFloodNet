9c150385c9348d16b201b144bd3c5e073eb48392
emideepak <79080929+emideepak@users.noreply.github.com>
2021-06-01 11:48:43 +0530

    Update ChangeDetection.py

--- Code/ChangeDetection.py
+++ Code/ChangeDetection.py
@@ -1,15 +1,15 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Program:To identify the changes due to flood on agriculture land
-Input: DecisionFusion output after flood (Combined_After_map.png)
-       DecisionFusion output before flood (Combined_Before_map.png)
+Input: DecisionFusion output after flood (Combined_After_Flood_map.png)
+       DecisionFusion output before flood (Combined_Before_Flood_map.png)
 Output: Change map of flood affected in agriculture land (Change_Map.png)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 from PIL import Image
 
 from pixelmatch.contrib.PIL import pixelmatch
 
-img_a = Image.open("Combined After map.png")
-img_b = Image.open("Combined Before map.png")
+img_a = Image.open("Combined_After_Flood_map.png")
+img_b = Image.open("Combined_Before_Flood_map.png")
 img_diff = Image.new("RGBA", img_a.size)
 
 # note how there is no need to specify dimensions
